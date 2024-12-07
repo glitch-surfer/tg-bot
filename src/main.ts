0c42f48bc0fee298e257d46e089442435788cb17
vladislav_garaev <vladislav_garaev@epam.com>
2024-12-08 00:50:14 +0100

    fix: token error

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const bot = new Bot(process.env.TOKEN);
+const bot = new Bot(process.env.TOKEN!);
 
 const randomWords = ["random word 1", "random word 2", "random word 3", "random word 4", "random word 5"];
 const getRandomWord = () => randomWords[Math.floor(Math.random() * randomWords.length)];
