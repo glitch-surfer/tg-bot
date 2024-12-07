3f6a62fd13ae2bb015f838f7acab4ab06f3bf8f1
vladislav_garaev <vladislav_garaev@epam.com>
2024-12-08 00:30:26 +0100

    init: add test bot implementation

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import {Bot} from "grammy";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const bot = new Bot(process.env.TOKEN);
+
+const randomWords = ["random word 1", "random word 2", "random word 3", "random word 4", "random word 5"];
+const getRandomWord = () => randomWords[Math.floor(Math.random() * randomWords.length)];
+
+bot.on("message", (ctx) => ctx.reply(getRandomWord()));
+
+bot.start();
