6b3d8f9070eec73322ca35bb8c04636d7e3a9979
vladislav_garaev <vladislav_garaev@epam.com>
2024-12-08 23:21:29 +0100

    feat: add easy words to bot

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import {Bot} from "grammy";
 import * as dotenv from "dotenv";
+import {readFile} from "node:fs/promises";
 
 dotenv.config();
 
+let easyWords: string[];
+try {
+    const content = await readFile("./src/easy-words.txt", "utf-8")
+    easyWords = content.split("\n")
+} catch (error) {
+    console.error(error);
+    process.exit(1);
+}
+
 const bot = new Bot(process.env.TOKEN!);
 
-const randomWords = ["random word 1", "random word 2", "random word 3", "random word 4", "random word 5"];
-const getRandomWord = () => randomWords[Math.floor(Math.random() * randomWords.length)];
+const getRandomWord = () => easyWords[Math.floor(Math.random() * easyWords.length)];
 
 bot.on("message", (ctx) => ctx.reply(getRandomWord()));
 
